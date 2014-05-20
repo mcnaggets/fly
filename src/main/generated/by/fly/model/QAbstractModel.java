b23cb001ccc54254839261b68f9b8135d986f7c0
m <mcnaggets@gmail.com>
2014-05-21 01:32:02 +0300

    mongodb support

---
+++ src/main/generated/by/fly/model/QAbstractModel.java
@@ -0,0 +1,37 @@
+package by.fly.model;
+
+import static com.mysema.query.types.PathMetadataFactory.*;
+
+import com.mysema.query.types.path.*;
+
+import com.mysema.query.types.PathMetadata;
+import javax.annotation.Generated;
+import com.mysema.query.types.Path;
+
+
+/**
+ * QAbstractModel is a Querydsl query type for AbstractModel
+ */
+@Generated("com.mysema.query.codegen.EmbeddableSerializer")
+public class QAbstractModel extends BeanPath<AbstractModel> {
+
+    private static final long serialVersionUID = -545503376L;
+
+    public static final QAbstractModel abstractModel = new QAbstractModel("abstractModel");
+
+    public final StringPath id = createString("id");
+
+    public QAbstractModel(String variable) {
+        super(AbstractModel.class, forVariable(variable));
+    }
+
+    public QAbstractModel(Path<? extends AbstractModel> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAbstractModel(PathMetadata<?> metadata) {
+        super(AbstractModel.class, metadata);
+    }
+
+}
+
