0582e4d6575c6e0c1f8f47caffdc73620980de5d
m <mcnaggets@gmail.com>
2014-05-23 12:09:57 +0300

    orders stuff+minor fixes

--- src/main/java/by/fly/repository/UserRepository.java
+++ src/main/java/by/fly/repository/UserRepository.java
@@ -4,6 +4,6 @@ import by.fly.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-public interface UserRepository extends MongoRepository<User, Long>, QueryDslPredicateExecutor {
+public interface UserRepository extends MongoRepository<User, String>, QueryDslPredicateExecutor {
     User findByBarcode(String barcode);
 }
--- src/main/java/by/fly/ui/controller/OrdersController.java
+++ src/main/java/by/fly/ui/controller/OrdersController.java
@@ -1,17 +1,35 @@
 package by.fly.ui.controller;
 
+import by.fly.model.OrderItem;
+import by.fly.repository.OrderItemRepository;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class OrdersController {
 
     public VBox orderTableRegion;
     public AnchorPane createOrderRegion;
+    public Label numberLabel;
+    public Label orderDateLabel;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     public void createOrder(ActionEvent actionEvent) {
+        OrderItem orderItem = new OrderItem(null, null);
+        orderItemRepository.save(orderItem);
+
+        numberLabel.setText("Заказ №" + orderItem.getId());
+        orderDateLabel.setText(DateTimeFormatter.ISO_LOCAL_DATE.format(LocalDate.now()));
+
         orderTableRegion.toBack();
         createOrderRegion.toFront();
     }
